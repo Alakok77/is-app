eb6de84a8749919a86a28d82dbfcf62f77a5172c
Alakok77 <top.p1234558@gmail.com>
2023-01-11 12:32:00 +0700

    rearange

--- lib/NavBar.dart
+++ lib/NavBar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Navbar extends  StatelessWidget {
+class Navbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -13,8 +13,8 @@ class Navbar extends  StatelessWidget {
             accountEmail: Text("alakokmanz@gmail.com"),
             currentAccountPicture: CircleAvatar(
                 child: ClipOval(
-              child: Image.network(
-                'https://us-fbcloud.net/wb/data/1317/1317466-img.uwv96d.8gxmo.jpg',
+              child: Image.asset(
+                'images/z1.png',
                 width: 90,
                 height: 90,
                 fit: BoxFit.cover,
@@ -22,9 +22,7 @@ class Navbar extends  StatelessWidget {
             )),
             decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: NetworkImage(
-                        'https://images.unsplash.com/photo-1538435740860-67bd8f4e8eb8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'),
-                    fit: BoxFit.cover)),
+                    image: AssetImage('images/z2.png'), fit: BoxFit.cover)),
           ),
           ListTile(
             title: Text('Developer\n     Alakokmanz \n      '),
